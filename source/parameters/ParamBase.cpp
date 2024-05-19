85cb7bbe576b99300e84f81dbad360e4d6336869
Butch Warns <contact@butchwarns.de>
2024-05-19 12:11:19 +0200

    Add license text to new source files

--- source/parameters/ParamBase.cpp
+++ source/parameters/ParamBase.cpp
@@ -1,3 +1,21 @@
+/*
+ParamBase.cpp
+Copyright (C) 2024 Butch Warns
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "ParamBase.h"
 
 const auto ParamBase::RANGE_DEFAULT = juce::NormalisableRange<float>(0.0f, 1.0f, 0.0000001f);
